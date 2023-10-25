a66367061376acb641dc335359ea05c0bf7697ed
fprzepio <132128402+fprzepio@users.noreply.github.com>
2023-10-25 10:54:09 +0200

    Update Program1.java

--- Program1.java
+++ Program1.java
@@ -3,13 +3,13 @@
 import javax.sound.midi.Soundbank;
 import java.util.Scanner;
 
-public class homework1 {
+public class intfactor {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.printf("Podaj liczbe calkowita: ");
+        System.out.printf("Insert and integer: ");
         int liczba = s.nextInt();
 
-        System.out.printf("Dzielnikiem liczby %d jest:%n", liczba);
+        System.out.printf("Factor of %d is:%n", liczba);
 
         for (int dzielnik = 1; dzielnik <= liczba; dzielnik++) {
             if (liczba % dzielnik == 0) {
